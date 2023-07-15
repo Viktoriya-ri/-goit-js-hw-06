b415e26a9f8cb83f2905ba2eb929ef92e411fd52
Вікторія <viktoriya23071@gmail.com>
2023-07-15 19:02:40 +0300

    s

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,12 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-const ingredientsRef = document.querySelector('#ingredients');
+const ingredientsRef= document.querySelector('#ingredients');
 
-const itemsRef = ingredients.map((ingredient) => {
+const items = ingredients.map((ingredient) => {
   const item = document.createElement('li');
   item.textContent = ingredient;
   item.classList.add('item');
   return item;
 })
-ingredientsRef.append(...itemsRef);
+ingredientsRef.append(...items);

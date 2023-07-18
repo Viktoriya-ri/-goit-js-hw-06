3d6f7386df03a0a7affb8e00de2261b440a33e5d
Вікторія <viktoriya23071@gmail.com>
2023-07-18 09:38:06 +0300

    s

--- src/js/task-07.js
+++
@@ -1,6 +0,0 @@
-const inputRef = document.querySelector('#font-size-control');
-const textRef = document.querySelector('#text');
-inputRef.addEventListener('input', () => {
-    const fontSize = inputRef.value + 'px';
-    textRef.style.fontSize = fontSize;
-})
